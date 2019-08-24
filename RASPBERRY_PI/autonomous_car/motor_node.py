b3f7071305c2fac260f437d53553aa5fba4418d5
GaParmar <gparmar@ucsd.edu>
2019-08-24 10:56:55 -0700

    motor node ready to be tested

--- RASPBERRY_PI/autonomous_car/motor_node.py
+++ RASPBERRY_PI/autonomous_car/motor_node.py
@@ -12,6 +12,15 @@ class ArduinoMotor:
             "direction" : "forward"
         }
         self.arduino_i2c_addr = 0x8
+        self.i2c_bus = SMBus(1)
+
+    def send_data(self):
+        # send the throttle first
+        data = [ord('t'), int(self.state["throttle"])]
+        self.i2c_bus.write_i2c_block_data(self.arduino_i2c_addr,0,data)
+        # senf the steering angle next
+        data = [ord('s'), int(self.state["steer"])]
+        self.i2c_bus.write_i2c_block_data(self.arduino_i2c_addr,0,data)
 
 if __name__ == "__main__":
     args = {
@@ -24,11 +33,15 @@ if __name__ == "__main__":
     
     # listen to relevant topic from socket server node
     rospy.Subscriber("update_throttle", Float32,
-                        lambda data: m.state["throttle"] = data.data)
+                        lambda data: m.state["throttle"] = int(data.data))
     rospy.Subscriber("update_direction", String,
                         lambda data: m.state["direction"] = data.data)
     rospy.Subscriber("update_steer_angle", Float32,
-                        lambda data: m.state["steer"] = data.data)
+                        lambda data: m.state["steer"] = int(data.data))
 
     while not rospy.is_shutdown():
-
+        start = time.time()
+        m.send_data()
+        # busy wait while frequency requirement is met
+        while(time.time()-start < args["rate"]):
+            pass
