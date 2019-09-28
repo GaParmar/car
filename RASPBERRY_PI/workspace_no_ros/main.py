c443d9c35d76e795ebc873f05b36dc87fc8361f8
GaParmar <gparmar@ucsd.edu>
2019-09-28 07:40:03 -0700

    camera test

--- RASPBERRY_PI/workspace_no_ros/main.py
+++ RASPBERRY_PI/workspace_no_ros/main.py
@@ -1,20 +1,37 @@
 import time
 from subprocess import Popen, PIPE 
 from multiprocessing.connection import Listener
+import socket
+import json
+import cv2
+import pdb
 
 if __name__ == "__main__":
-    d_listeners = {}
-    d_conns = {}
+
+    socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket.bind(('', 8080))
+
+    cv2.destroyAllWindows()
+    device = cv2.VideoCapture(0)
 
     # start the process listening to incoming socket
-    process = Popen(['python3', './socket_process.py'])
-    d_listeners["socket"] = Listener(('localhost', 6000), authkey=b"secret")
-    d_conns["socket"] = d_listeners["socket"].accept()
+    # d_process["socket"] = Popen(['python3', './socket_process.py'])
+    # d_listeners["socket"] = Listener(('localhost', 6000), authkey=b"secret")
+    # d_conns["socket"] = d_listeners["socket"].accept()
+        # data = d_conns["socket"].recv()
+        # print(data)
 
     # the main loop
     while True:
         start = time.time()
-        data = d_conns["socket"].recv()
-        print(data)
+
+        data_raw,addr = socket.recvfrom(1024)
+        data = json.loads(data_raw)
+
+        status, img = device.read()
+        
+        pdb.set_trace()
+
+
         while time.time()-start < 1:
             pass
