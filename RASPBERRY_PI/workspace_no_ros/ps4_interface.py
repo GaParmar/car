edfce9b756f2b5a8b10314911631f87b4e6f8ae6
Cyrus Cowley <cyficowley@gmail.com>
2019-10-08 19:37:44 -0700

    added a ps4 controller class

---
+++ RASPBERRY_PI/workspace_no_ros/ps4_interface.py
@@ -0,0 +1,55 @@
+import evdev
+from evdev import InputDevice, categorize, ecodes
+import asyncio
+from multiprocessing import Process, Manager
+
+
+def update_inputs(dev, data):
+    async def update_inputs(dev, data):
+        async for event in dev.async_read_loop():
+            if event.type == 1:
+                if(event.code == 304):
+                    data["cross"] = event.value
+                if(event.code == 308):
+                    data["square"] = event.value
+                if(event.code == 307):
+                    data["triangle"] = event.value
+                if(event.code == 305):
+                    data["circle"] = event.value
+
+            # 0 to 255
+            elif(event.type == 3):
+                if(event.code == 0):
+                    data["lx"] = event.value
+                elif(event.code == 1):
+                    data["ly"] = event.value
+                elif(event.code == 3):
+                    data["rx"] = event.value
+                elif(event.code == 4):
+                    data["ry"] = event.value
+            
+        
+    asyncio.ensure_future(update_inputs(dev, data))
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
+
+
+class PS4Interface:
+    def __init__(self):
+        manager = Manager()
+        data = manager.dict({"cross": 0, "square": 0, "triangle": 0, "circle": 0, "lx": 128, "ly": 128, "rx": 128, "ry": 128})
+
+        self.data = data
+
+        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+
+        path = None
+
+        for device in devices:
+            if(device.name == "Wireless Controller"):
+                path = device.path
+
+        dev = InputDevice(path)
+
+        socket_process = Process(target=update_inputs, args=(dev, self.data))
+        socket_process.start()
